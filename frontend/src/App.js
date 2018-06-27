a544eb712273f94359cb169f54bd98eb9face7b7
zach <zaspringer@gmail.com>
2018-06-27 15:07:54 -0700

    Started on tutorial by fun fun function to determine point above line or below line

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { range } from 'ramda';
+
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      numbers: []
+    }
+  }
+  componentDidMount() {
+    if(this.state.numbers.length === 0) {
+      var rand = (high, low) => { return (Math.random() * (high - low) + low)};
+      var x_max = 400;
+      var y_max = 400;
+      var randomPoints = range(0, 100).map(_ => ({
+        x: rand(0, x_max),
+        y: rand(0, y_max)
+      }))
+      this.setState({
+        numbers: randomPoints
+      })
+    }
+
+  }
+
+  team(point) {
+    if(point)
+  }
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +39,19 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <svg width="450" height="450">
+           {
+             this.state.numbers.map((point, i) => {
+               return <circle
+                 cx={point.x}
+                 cy={point.y}
+                 r="5"
+                 key={i}
+                 />
+             })
+           }
+           <line x1="0" x2="450" y1="0" y2="450" stroke="purple"></line>
+          </svg>
         </p>
       </div>
     );
